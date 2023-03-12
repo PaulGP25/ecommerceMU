fc32df1cbc4da2a8cc4d1c918bd9932eab8e0ff9
PaulGP25 <prego.paul25@gmail.com>
2023-03-12 18:54:18 -0500

    Beggining

---
+++ src/main/java/com/ecommerce/mu/EcommerceMuApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.mu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMuApplication.class, args);
+	}
+
+}
